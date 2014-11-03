fb5d593c2027eea5ac82aad49d50e2baa3ab97b2
Luis <luis@luis-VirtualBox.(none)>
2014-11-03 12:22:15 -0800

    Cleaning up.

--- Library.py
+++ Library.py
@@ -1,12 +1,14 @@
 ## Set of library functions
 
 import galsim
+import numpy as np
+import lmfit
 
 # Create a galaxy with a sersic profile and optional psf to the image. 
 def create_galaxy(flux, hlr, e1, e2, x0, y0, galtype_gal=galsim.Sersic, sersic_index=0.5,
                   psf_flag=False, psf_type=galsim.Moffat, beta=5, size_psf=1, flux_psf=1,
                   x_len=100, y_len=100, scale=0.2, method='fft',seed=None,
-                  verbose=False, max_fft_size=100000, return_obj=False):
+                  verbose=False, max_fft_size=10000000, return_obj=False):
                   
     big_fft_params = galsim.GSParams(maximum_fft_size=max_fft_size)
     
@@ -54,8 +56,6 @@ def convolve_with_psf(gal, beta, size_psf, psf_type=galsim.Moffat, flux_psf=1,
 
 # Add poisson noise to an image with a sky level argument.        
 def add_noise(image, noise_type=galsim.PoissonNoise, seed=None, sky_level=0):
-    if sky_level == 0: 
-        print "Adding Poisson noise without sky level."     
     if noise_type is galsim.PoissonNoise:    
         image.addNoise(noise_type(sky_level=sky_level,rng=seed))
         return image
@@ -77,6 +77,96 @@ def residual_1_obj(param, data_image, sky_level, x_len, y_len, pixel_scale,
                           x_len=x_len,y_len=y_len,scale=pixel_scale)
     
     return (image-data_image).array.ravel()
+
+# Residual function for fitting two objects to data.    
+def residual_func_simple(param, data_image, sky_level, x_len, y_len, pixel_scale, 
+                         galtype_a,n_a,galtype_b,n_b):
+        
+    assert galtype_a != None
+    assert galtype_b != None
+    flux_a = param['flux_a'].value
+    hlr_a = param['hlr_a'].value
+    e1_a = param['e1_a'].value
+    e2_a = param['e2_a'].value
+    x0_a = param['x0_a'].value
+    y0_a = param['y0_a'].value
+
+    flux_b = param['flux_b'].value
+    hlr_b = param['hlr_b'].value
+    e1_b = param['e1_b'].value
+    e2_b = param['e2_b'].value
+    x0_b = param['x0_b'].value
+    y0_b = param['y0_b'].value
+    image_a = create_galaxy(flux_a,hlr_a,e1_a,e2_a,x0_a,y0_a,galtype_gal=galtype_a,sersic_index=n_a,
+                            x_len=x_len,y_len=y_len,scale=pixel_scale)
+                            
+    image_b = create_galaxy(flux_b,hlr_b,e1_b,e2_b,x0_b,y0_b,galtype_gal=galtype_b,sersic_index=n_b,
+                            x_len=x_len,y_len=y_len,scale=pixel_scale)
+                        
+    image = image_a + image_b
+    
+    if sky_level > 10:        
+        return (data_image-image).array.ravel()/np.sqrt(sky_level + image.array).ravel()
+    else:
+        return (data_image-image).array.ravel()
+        
+# Function definition to return the original data array, best-fit array,
+# residual, and correlation matrix with differences and error on e1 and e2.
+def run_2_galaxy_full_params_simple(flux_a,hlr_a,e1_a,e2_a,x0_a,y0_a,n_a,
+                                    flux_b,hlr_b,e1_b,e2_b,x0_b,y0_b,n_b,
+                                    psf_flag,beta,fwhm_psf,
+                                    x_len,y_len,pixel_scale,galtype_a,galtype_b,seed_a,seed_b,seed_p,
+                                    add_noise_flag,sky_level):
+
+    image_a = create_galaxy(flux_a,hlr_a,e1_a,e2_a,x0_a,y0_a,galtype_gal=galtype_a,sersic_index=n_a,
+                            x_len=x_len,y_len=y_len,scale=pixel_scale,
+                            psf_flag=psf_flag, beta=beta, size_psf=fwhm_psf)
+                                
+    image_b = create_galaxy(flux_b,hlr_b,e1_b,e2_b,x0_b,y0_b,galtype_gal=galtype_b,sersic_index=n_b,
+                            x_len=x_len,y_len=y_len,scale=pixel_scale,
+                            psf_flag=psf_flag, beta=beta, size_psf=fwhm_psf)
+    
+    image_no_noise = image_a + image_b                        
+    image = image_a + image_b
+    if add_noise_flag == True:
+        image_noise = add_noise(image,seed=seed_p,sky_level=sky_level)
+        image = image_noise
+    else:
+        image_noise = image
+    
+    # -----------------------------------------------------------------------    
+    # Estimate the parameters of the image.
+    
+    # Define some seed that's far from true values and insert into
+    # lmfit object for galaxy one and two
+    lim = 1/np.sqrt(2)
+    p0 = 1.0*np.array([flux_a,hlr_a,e1_a,e2_a,x0_a,y0_a,
+                       flux_b,hlr_b,e1_b,e2_b,x0_b,y0_b])
+    parameters = lmfit.Parameters()
+    parameters.add('flux_a', value=p0[0])
+    parameters.add('hlr_a', value=p0[1])
+    #parameters.add('e1_a', value=p0[2],min=-0.99,max=0.99)
+    #parameters.add('mag',value=0.99,max=0.99,vary=True)
+    #parameters.add('e2_a',expr='sqrt(mag**2 - e1_a**2)')
+    parameters.add('e1_a',value=p0[2],min=-lim,max=lim)
+    parameters.add('e2_a',value=p0[3],min=-lim,max=lim)    
+    parameters.add('x0_a',value=p0[4])
+    parameters.add('y0_a',value=p0[5])
+    
+    parameters.add('flux_b', value=p0[6])
+    parameters.add('hlr_b', value=p0[7])
+    parameters.add('e1_b',value=p0[8],min=-lim,max=lim)
+    parameters.add('e2_b',value=p0[9],min=-lim,max=lim)
+    #parameters.add('e1_b', value=p0[2],min=-0.99,max=0.99)
+    #parameters.add('e2_b',expr='sqrt(mag**2 - e1_b**2)')
+    parameters.add('x0_b',value=p0[10])
+    parameters.add('y0_b',value=p0[11])
+    
+    
+    # Extract params that minimize the difference of the data from the model.
+    result = lmfit.minimize(residual_func_simple, parameters, args=(image, sky_level, x_len, y_len, pixel_scale, galtype_a, n_a, galtype_b, n_b))                                   
+                                                                      
+    return image_no_noise, image_noise, best_fit_a+best_fit_b, result
 
 # Convert python file to ipython notebook document format.    
 def to_ipynb(infile,outfile):
