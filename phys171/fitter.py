f30a2534c25527ba91445832632a019e3b5a9c15
Luis <luis@luis-VirtualBox.(none)>
2015-03-12 22:21:59 -0700

    Added fitter.

--- phys171/fitter.py
+++ phys171/fitter.py
@@ -16,7 +16,8 @@ import matplotlib.pyplot as plt
 import pdb
     
 def model(beta,data):
-    return 1/(np.array(xrange(0,len(data)))**(beta))
+    x = np.linspace(1,len(data),len(data))
+    return 1/(x**(beta))
 
 # Take the difference of the data and the model for one galaxy
 def resid(param, data):
@@ -25,19 +26,33 @@ def resid(param, data):
     true[0] = 1
     return (true-data)
     
+def correlation_fit(data):
+    parameters = lmfit.Parameters()
+    parameters.add('beta', value=(1/4))
+    # Extract params that minimize the difference of the data from the true.
+    result = lmfit.minimize(resid, parameters, args=[data])
+    return model(result.values['beta'],data)
+    
 
 if __name__ == '__main__':
     data = pd.read_csv('data/two_point_met1600_2000.csv')
-    crit_temp_data = data['2.26']
-    crit_temp_refined = crit_temp_data[0:38].values
+    crit_temp_data = np.abs(data['2.26'])
+    refined_data = data[0:33]
+    crit_temp_refined = crit_temp_data[0:33].values
     parameters = lmfit.Parameters()
     parameters.add('beta', value=(1/4))
     # Extract params that minimize the difference of the data from the true.
     result = lmfit.minimize(resid, parameters, args=[crit_temp_refined])
     
     domain = np.array(xrange(0,len(crit_temp_refined)))
+    fit = model(result.values['beta'],crit_temp_refined)
     plt.plot(domain,crit_temp_refined,'--o',
-             domain,model(result.values['beta'],crit_temp_refined),'--o',
+             domain,fit,'--o',
              domain,model(1/4,crit_temp_refined),'--o')
              
     plt.legend(['Data','Fit','True'])
+    
+    plt.figure()
+    cor_leng = np.array([0.02,0.0015,0.017,fit[len(fit)-1],refined_data['2.33'].values[32],refined_data['2.4'].values[32]])
+    plt.plot([2.06,2.13,2.20,2.26,2.33,2.4],-40/np.log(cor_leng),'--o')
+    
